671f7e3e92f71ff285793115b39140229061912c
Victor <arr.chaos@gmail.com>
2022-08-04 12:27:52 +0300

    first commit

---
+++ tag-manager.php
@@ -0,0 +1,18 @@
+<?php 
+
+echo <<<END
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'www.googletagmanager.com/gtm.js^id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TK74V9J');</script>
+<!-- End Google Tag Manager -->
+</head>
+<body class="bg-shape">
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TK74V9J"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+<!--================ Header Menu Area start =================-->
+END;
