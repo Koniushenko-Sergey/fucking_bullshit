7c196226eed28668112501d4db3523bb80a7cd14
Victor <arr.chaos@gmail.com>
2022-08-04 12:54:41 +0300

    add any pages

---
+++ about/index.php
@@ -0,0 +1,150 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>About Us | TargetTopSlots.com</title>
+	<meta name="description" content="desc">
+	<link rel="icon" href="img/favicon.ico" type="image/ico">
+
+  <link rel="stylesheet" href="vendors/bootstrap/bootstrap.min.css">
+  <link rel="stylesheet" href="vendors/fontawesome/css/all.min.css">
+  <link rel="stylesheet" href="vendors/themify-icons/themify-icons.css">
+  <link rel="stylesheet" href="vendors/linericon/style.css">
+  <link rel="stylesheet" href="vendors/owl-carousel/owl.theme.default.min.css">
+  <link rel="stylesheet" href="vendors/owl-carousel/owl.carousel.min.css">
+  <link rel="stylesheet" href="vendors/flat-icon/font/flaticon.css">
+  <link rel="stylesheet" href="vendors/nice-select/nice-select.css">
+  <link rel="stylesheet" href="css/purecookie.css">
+  <link rel="stylesheet" href="css/style.css">
+  <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TK74V9J');</script>
+<!-- End Google Tag Manager -->
+</head>
+<body class="bg-shape">
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TK74V9J"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+  <!--================ Header Menu Area start =================-->
+  <header class="header_area">
+    <div class="main_menu">
+      <nav class="navbar navbar-expand-lg navbar-light">
+        <div class="container box_1620">
+          <a class="navbar-brand logo_h" href="index.html"><img src="img/logo%EF%B9%96v=1.png" alt=""></a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+
+          <div class="collapse navbar-collapse offset" id="navbarSupportedContent">
+            <ul class="nav navbar-nav menu_nav justify-content-end">
+             <li class="nav-item">
+					<a class="nav-link" href="about.html">About Us</a>
+				  </li>
+				  <li class="nav-item">
+					<a class="nav-link" href="cookie-policy.html">Cookie Policy</a>
+				  </li>
+				  <li class="nav-item">
+					<a class="nav-link" href="privacy-policy.html">Privacy Policy</a>
+				  </li>
+				  <li class="nav-item">
+					<a class="nav-link" href="terms-and-conditions.html">Terms & Conditions</a>
+				  </li>
+                	
+              
+            </ul>
+
+            <div class="nav-right text-center text-lg-right py-4 py-lg-0">
+             
+                <a href="register.html"><button class="button button-header bg">Sign up</button></a>
+				            </div>
+          </div> 
+        </div>
+      </nav>
+    </div>
+  </header>
+<section class="hero-banner-sm magic-ball magic-ball-banner" id="parallax-1" data-anchor-target="#parallax-1" data-300-top="background-position: 0px -80px" data-top-bottom="background-position: 0 100px">
+    <div class="container">
+      <div class="hero-banner-sm-content">
+        <h1>About Us</h1>
+        <nav aria-label="breadcrumb" class="banner-breadcrumb" style="color:black;">
+          <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="index.html" style="color:black;">Home</a></li>
+            <li class="breadcrumb-item active" aria-current="page" style="color:black;">About Us</li>
+          </ol>
+        </nav>
+      </div>
+    </div>
+  </section>
+  
+ 
+	
+	
+        <section class="section-margin">
+	<div class="container section-intro text-center pb-90px">
+                <div class="row">
+                    
+                            
+						<p>
+						Our team has always been focused on creating place where players can find best social games. You can find best games to play for free and start enjoying immediately. <br/>We do not use real money concept; therefore, everything is free of charge. Our game offers to the players sensational bonuses, attractive virtual enjoyment. Safety and security in our apps and other online products are fully guaranteed. <br/>By playing our social games you can see how well you are doing in your online gaming skills. All games in our web site are totally free to play either it is slots game or table game.<br/><br/>The games we recommend here are product of best teams in this industry. Games we offer are made WMS, Konami, IGT, Bally, and Aristocrat (these are main slots makers for land-based casinos. <br/>						</p>
+			
+                </div>
+	</div>
+</section>
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+	
+	
+	  <footer class="footer-area section-gap">
+		<div class="container" style="margin-top:-12%;"><br><br>
+		    <div class="col-md-12 text-center">
+            <p style="padding-left:15px;padding-right:15px;"><br>&copy; Our games are intended only for adult audience (18+). Real money gambling or an opportunity to win real money or any type of prizes are never offere.</p>	
+			<a href="https://www.begambleaware.org/"><img src="img/gambleaware.png" alt="GambleAware"></a>
+			</div>
+			<div class="footer-bottom row align-items-center text-center text-lg-left" style="border-top:0px;">
+				<p class="footer-text col-lg-8 col-md-12" style="border-top:0px;">
+&copy;<script>document.write(new Date().getFullYear());</script> <b><a href="index.html">TargetTopSlots.com</a> - All Rights Are Reserved.</b>
+</p>
+				<div class="col-lg-4 col-md-12 text-center text-lg-right">
+				<p>
+				<a href="privacy-policy.html">Privacy Policy</a> |
+				<a href="cookie-policy.html">Cookie Policy</a> | 
+				<a href="terms-and-conditions.html">Terms & Conditions</a>
+				</p>  
+				</div>
+			</div>
+		</div>
+	</footer>
+
+
+  <script src="js/purecookie.js"></script>
+  <script src="vendors/jquery/jquery-3.2.1.min.js"></script>
+  <script src="vendors/bootstrap/bootstrap.bundle.min.js"></script>
+  <script src="vendors/owl-carousel/owl.carousel.min.js"></script>
+  <script src="js/jquery.ajaxchimp.min.js"></script>
+  <script src="js/mail-script.js"></script>
+  <script src="js/main.js"></script>
+</body>
+</html>
---
+++ cookie-policy/index.php
@@ -0,0 +1,251 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Cookie Policy | TargetTopSlots.com</title>
+	<meta name="description" content="desc">
+	<link rel="icon" href="img/favicon.ico" type="image/ico">
+
+  <link rel="stylesheet" href="vendors/bootstrap/bootstrap.min.css">
+  <link rel="stylesheet" href="vendors/fontawesome/css/all.min.css">
+  <link rel="stylesheet" href="vendors/themify-icons/themify-icons.css">
+  <link rel="stylesheet" href="vendors/linericon/style.css">
+  <link rel="stylesheet" href="vendors/owl-carousel/owl.theme.default.min.css">
+  <link rel="stylesheet" href="vendors/owl-carousel/owl.carousel.min.css">
+  <link rel="stylesheet" href="vendors/flat-icon/font/flaticon.css">
+  <link rel="stylesheet" href="vendors/nice-select/nice-select.css">
+  <link rel="stylesheet" href="css/purecookie.css">
+  <link rel="stylesheet" href="css/style.css">
+  <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TK74V9J');</script>
+<!-- End Google Tag Manager -->
+</head>
+<body class="bg-shape">
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TK74V9J"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+  <!--================ Header Menu Area start =================-->
+  <header class="header_area">
+    <div class="main_menu">
+      <nav class="navbar navbar-expand-lg navbar-light">
+        <div class="container box_1620">
+          <a class="navbar-brand logo_h" href="index.html"><img src="img/logo%EF%B9%96v=1.png" alt=""></a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+
+          <div class="collapse navbar-collapse offset" id="navbarSupportedContent">
+            <ul class="nav navbar-nav menu_nav justify-content-end">
+             <li class="nav-item">
+					<a class="nav-link" href="about.html">About Us</a>
+				  </li>
+				  <li class="nav-item">
+					<a class="nav-link" href="cookie-policy.html">Cookie Policy</a>
+				  </li>
+				  <li class="nav-item">
+					<a class="nav-link" href="privacy-policy.html">Privacy Policy</a>
+				  </li>
+				  <li class="nav-item">
+					<a class="nav-link" href="terms-and-conditions.html">Terms & Conditions</a>
+				  </li>
+                	
+              
+            </ul>
+
+            <div class="nav-right text-center text-lg-right py-4 py-lg-0">
+             
+                <a href="register.html"><button class="button button-header bg">Sign up</button></a>
+				            </div>
+          </div> 
+        </div>
+      </nav>
+    </div>
+  </header>
+	<section class="hero-banner-sm magic-ball magic-ball-banner" id="parallax-1" data-anchor-target="#parallax-1" data-300-top="background-position: 0px -80px" data-top-bottom="background-position: 0 100px">
+    <div class="container">
+      <div class="hero-banner-sm-content">
+        <h1>Cookie Policy</h1>
+        <nav aria-label="breadcrumb" class="banner-breadcrumb" style="color:black;">
+          <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="index.html" style="color:black;">Home</a></li>
+            <li class="breadcrumb-item active" aria-current="page" style="color:black;">Cookie Policy</li>
+          </ol>
+        </nav>
+      </div>
+    </div>
+  </section>
+  
+ 
+	
+	
+        <section class="section-margin">
+	<div class="container section-intro pb-90px">
+                <div class="row">
+             <div class="col-md-12 col-sm-12 col-xs-12">       
+                        
+
+<p>This is the Cookie Policy for TargetTopSlots.com, accessible from https://targettopslots.com</p>
+
+<br><p><strong>What Are Cookies</strong></p>
+
+<p>As is common practice with almost all professional websites this site uses cookies, which are tiny files that are downloaded to your computer, to improve your experience. This page describes what information they gather, how we use it and why we sometimes need to store these cookies. We will also share how you can prevent these cookies from being stored however this may downgrade or 'break' certain elements of the sites functionality.</p>
+
+<p>For more general information on cookies, please read <a href="https://www.cookieconsent.com/what-are-cookies/">"What Are Cookies"</a>.</p>
+
+<br><p><strong>How We Use Cookies</strong></p>
+
+<p>We use cookies for a variety of reasons detailed below. Unfortunately in most cases there are no industry standard options for disabling cookies without completely disabling the functionality and features they add to this site. It is recommended that you leave on all cookies if you are not sure whether you need them or not in case they are used to provide a service that you use.</p>
+
+<br><p><strong>Disabling Cookies</strong></p>
+
+<p>You can prevent the setting of cookies by adjusting the settings on your browser (see your browser Help for how to do this). Be aware that disabling cookies will affect the functionality of this and many other websites that you visit. Disabling cookies will usually result in also disabling certain functionality and features of the this site. Therefore it is recommended that you do not disable cookies.</p>
+
+<br><p><strong>The Cookies We Set</strong></p>
+
+<ul>
+
+<li>
+    <p>Account related cookies</p>
+    <p>If you create an account with us then we will use cookies for the management of the signup process and general administration. These cookies will usually be deleted when you log out however in some cases they may remain afterwards to remember your site preferences when logged out.</p>
+</li>
+
+<li>
+    <p>Login related cookies</p>
+    <p>We use cookies when you are logged in so that we can remember this fact. This prevents you from having to log in every single time you visit a new page. These cookies are typically removed or cleared when you log out to ensure that you can only access restricted features and areas when logged in.</p>
+</li>
+
+<li>
+    <p>Email newsletters related cookies</p>
+    <p>This site offers newsletter or email subscription services and cookies may be used to remember if you are already registered and whether to show certain notifications which might only be valid to subscribed/unsubscribed users.</p>
+</li>
+
+<li>
+    <p>Orders processing related cookies</p>
+    <p>This site offers e-commerce or payment facilities and some cookies are essential to ensure that your order is remembered between pages so that we can process it properly.</p>
+</li>
+
+<li>
+    <p>Surveys related cookies</p>
+    <p>From time to time we offer user surveys and questionnaires to provide you with interesting insights, helpful tools, or to understand our user base more accurately. These surveys may use cookies to remember who has already taken part in a survey or to provide you with accurate results after you change pages.</p>
+</li>
+
+<li>
+    <p>Forms related cookies</p>
+    <p>When you submit data to through a form such as those found on contact pages or comment forms cookies may be set to remember your user details for future correspondence.</p>
+</li>
+
+<li>
+    <p>Site preferences cookies</p>
+    <p>In order to provide you with a great experience on this site we provide the functionality to set your preferences for how this site runs when you use it. In order to remember your preferences we need to set cookies so that this information can be called whenever you interact with a page is affected by your preferences.</p>
+</li>
+
+</ul>
+
+<br><p><strong>Third Party Cookies</strong></p>
+
+<p>In some special cases we also use cookies provided by trusted third parties. The following section details which third party cookies you might encounter through this site.</p>
+
+<ul>
+
+
+<li>
+    <p>Third party analytics are used to track and measure usage of this site so that we can continue to produce engaging content. These cookies may track things such as how long you spend on the site or pages you visit which helps us to understand how we can improve the site for you.</p>
+</li>
+
+<li>
+    <p>From time to time we test new features and make subtle changes to the way that the site is delivered. When we are still testing new features these cookies may be used to ensure that you receive a consistent experience whilst on the site whilst ensuring we understand which optimisations our users appreciate the most.</p>
+</li>
+
+<li>
+    <p>As we sell products it's important for us to understand statistics about how many of the visitors to our site actually make a purchase and as such this is the kind of data that these cookies will track. This is important to you as it means that we can accurately make business predictions that allow us to monitor our advertising and product costs to ensure the best possible price.</p>
+</li>
+
+
+<li>
+    <p>We use adverts to offset the costs of running this site and provide funding for further development. The behavioural advertising cookies used by this site are designed to ensure that we provide you with the most relevant adverts where possible by anonymously tracking your interests and presenting similar things that may be of interest.</p>
+</li>
+
+
+<li>
+    <p>Several partners advertise on our behalf and affiliate tracking cookies simply allow us to see if our customers have come to the site through one of our partner sites so that we can credit them appropriately and where applicable allow our affiliate partners to provide any bonus that they may provide you for making a purchase.</p>
+</li>
+
+<li>
+    <p>We also use social media buttons and/or plugins on this site that allow you to connect with your social network in various ways. For these to work the following social media sites including; {List the social networks whose features you have integrated with your site?:12}, will set cookies through our site which may be used to enhance your profile on their site or contribute to the data they hold for various purposes outlined in their respective privacy policies.</p>
+</li>
+
+</ul>
+
+<br><p><strong>More Information</strong></p>
+
+<p>Hopefully that has clarified things for you and as was previously mentioned if there is something that you aren't sure whether you need or not it's usually safer to leave cookies enabled in case it does interact with one of the features you use on our site. </p>
+
+<p>However if you are still looking for more information then you can contact us through one of our preferred contact methods:</p>
+
+<ul>
+<li>Email: <a href="cdn-cgi/l/email-protection.html" class="__cf_email__" data-cfemail="addfc8caedd9ccdfcac8d9d9c2dddec1c2d9de83cec2c0">[email&#160;protected]</a></li>
+
+</ul>
+
+			</div>
+                </div>
+	</div>
+</section>
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+	
+	
+	  <footer class="footer-area section-gap">
+		<div class="container" style="margin-top:-12%;"><br><br>
+		    <div class="col-md-12 text-center">
+            <p style="padding-left:15px;padding-right:15px;"><br>&copy; Our games are intended only for adult audience (18+). Real money gambling or an opportunity to win real money or any type of prizes are never offere.</p>	
+			<a href="https://www.begambleaware.org/"><img src="img/gambleaware.png" alt="GambleAware"></a>
+			</div>
+			<div class="footer-bottom row align-items-center text-center text-lg-left" style="border-top:0px;">
+				<p class="footer-text col-lg-8 col-md-12" style="border-top:0px;">
+&copy;<script data-cfasync="false" src="cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script>document.write(new Date().getFullYear());</script> <b><a href="index.html">TargetTopSlots.com</a> - All Rights Are Reserved.</b>
+</p>
+				<div class="col-lg-4 col-md-12 text-center text-lg-right">
+				<p>
+				<a href="privacy-policy.html">Privacy Policy</a> |
+				<a href="cookie-policy.html">Cookie Policy</a> | 
+				<a href="terms-and-conditions.html">Terms & Conditions</a>
+				</p>  
+				</div>
+			</div>
+		</div>
+	</footer>
+
+
+  <script src="js/purecookie.js"></script>
+  <script src="vendors/jquery/jquery-3.2.1.min.js"></script>
+  <script src="vendors/bootstrap/bootstrap.bundle.min.js"></script>
+  <script src="vendors/owl-carousel/owl.carousel.min.js"></script>
+  <script src="js/jquery.ajaxchimp.min.js"></script>
+  <script src="js/mail-script.js"></script>
+  <script src="js/main.js"></script>
+</body>
+</html>
