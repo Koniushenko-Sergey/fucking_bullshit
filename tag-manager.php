2485cdf7f5ba2d32531338be0011a1f2640197e2
Victor <arr.chaos@gmail.com>
2022-08-10 01:09:00 +0300

    imgs

--- tag-manager.php
+++ tag-manager.php
@@ -1,18 +1,16 @@
-<?php 
+<?php
 
 echo <<<END
 <!-- Google Tag Manager -->
 <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'www.googletagmanager.com/gtm.js^id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-TK74V9J');</script>
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KXJCVPK');</script>
 <!-- End Google Tag Manager -->
-</head>
-<body class="bg-shape">
+
 <!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TK74V9J"
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KXJCVPK"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <!-- End Google Tag Manager (noscript) -->
-<!--================ Header Menu Area start =================-->
 END;
